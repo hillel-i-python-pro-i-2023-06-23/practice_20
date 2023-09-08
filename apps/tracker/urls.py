b8e5d73d2edc62070777f13bcfb243cb58b4d9e8
YevhenYalovenko <jenyaya0007@gmail.com>
2023-09-08 12:04:29 +0300

    Add views and templates for wallet

--- apps/tracker/urls.py
+++ apps/tracker/urls.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import register_login
+from .views.register_login import register_view, login_view
+from .views.wallet import WalletCreateView, WalletListView, WalletDetailView
 
 app_name = "tracker"
 
 urlpatterns = [
-    path("register/", register_login.register_view, name="register"),
-    path("login/", register_login.login_view, name="login"),
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("create/", WalletCreateView.as_view(), name="wallet_create"),
+    path("list/", WalletListView.as_view(), name="wallet_list"),
+    path("detail/<int:pk>", WalletDetailView.as_view(), name="wallet_detail"),
 ]
